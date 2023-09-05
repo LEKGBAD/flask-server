3f6afa284ac4eaaba485e117e8c454e7f361376d
LEKGBAD <68630791+LEKGBAD@users.noreply.github.com>
2023-09-05 08:37:25 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 CORS(app);
 @app.route("/",methods=["GET"])
 def home():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return jsonify({"message":"obe eja"})
 @app.route("/extapi/home",methods=['GET'])
 def homeApi():
     return jsonify({"message":"Flask greets u"})
