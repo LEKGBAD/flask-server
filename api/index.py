fcfde024b066357cf72a2dbe4a3a193d68bb5e96
LEKGBAD <68630791+LEKGBAD@users.noreply.github.com>
2023-09-05 09:18:04 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,12 +3,15 @@ from flask_cors import CORS
 
 app=Flask(__name__)
 CORS(app);
+
 @app.route("/",methods=["GET"])
 def home():
     return render_template('index.html')
-@app.route('/hello')
+    
+@app.route('/hello',methods=['GET', 'POST', 'PUT'])
 def hello():
-    return render_template('index.html')
+    # return render_template('index.html')
+    pass
 
 
 # if __name__ == '__main__':
