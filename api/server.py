e951f7633ccad69655ef5cd686095ef46680825f
LEKGBAD <68630791+LEKGBAD@users.noreply.github.com>
2023-09-05 07:52:22 +0100

    Initial commit

---
+++ api/server.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify,render_template
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app);
+@app.route("/",methods=["GET"])
+def home():
+    return render_template('index.html')
+@app.route("/extapi/home",methods=['GET'])
+def homeApi():
+    return jsonify({"message":"Flask greets u"})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True,port="8080")
